fc3ab883703917a49f8e1ad2aaf546244a5bafaa
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-08-05 11:43:55 -0400

    Basic boiler plate setup

---
+++ config.js
@@ -0,0 +1,24 @@
+config = {
+	origins: 'http://localhost:3000',
+	db: 'mongodb://localhost:27017/api5.0',
+	secret: 'IfIspinaroundclockwiseforsomereason,Ihavetospinaroundcounterclockwiserightawaysoastounwindmyself.',
+}
+
+if (process.env.NODE_ENV == 'production') {
+	config.db = process.env.MONGODB_URI
+	config.secret = process.env.SECRET
+	config.origins = 'http://localhost:3000 http://localhost:3000'
+}
+
+config.options = function (origin, callback) {
+	if (!origin) {
+		callback(null, true)
+	}
+	if (config.allowedOrigins.includes(origin)) {
+		callback(null, true)
+	} else {
+		callback(new Error('Not allowed by CORS'))
+	}
+}
+
+module.exports = config
